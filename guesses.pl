99bc7063157d11d46ed90e02848f36ae800cfb23
Fisher Adelakin <temidayoadelakin@gmail.com>
2013-03-17 15:02:20 -0300

    Fixed the loop and added somethings
    I fixed the loop to keep going until the right answer is guessed and I added some other stuff

--- guesses.pl
+++ guesses.pl
@@ -1,7 +1,12 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
 my $secret = int(1 + rand 100);
 
 # You may uncomment this line during debugging
-# print "Don't tell anyone, but the secret number is $secret.\n";
+# say "Don't tell anyone, but the secret number is $secret.\n";
 
 while (1) {
 	print "Please enter a guess from 1 to 100: ";
@@ -11,11 +16,12 @@ while (1) {
 		last;
 	} elsif ($guess < $secret) {
 		print "Too small. Try again!\n";
-		last;
+		redo;
 	} elsif ($guess == $secret) {
-		print "That was it!\n"
+		print "That was it!\n";
 		last;
 	} else {
 		print "Too large. Try again!\n";
+		redo;
 	}
 }
